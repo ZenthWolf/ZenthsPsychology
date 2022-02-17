a33d7c6d5709445d36e3544eca0413e2292b7789
ZenthWolf <ZenthRelin@gmail.com>
2022-02-16 20:11:01 -0500

    Add project files.

---
+++ Source/Zenth's Psychology/ZPsychPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace ZPsych
+{
+    [StaticConstructorOnStartup]
+    public static class ZPsychPatcher
+    {
+        static ZPsychPatcher()
+        {
+            var harmony = new Harmony("ZenthWolf.Psych");
+            Log.Message("Hello, Psych.");
+        }
+    }
+}
